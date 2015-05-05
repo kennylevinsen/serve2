3e2e305ac651e6a425804b0032be724a49e3d28c
Kenny Levinsen <kl_online@icloud.com>
2015-05-06 01:03:57 +0200

    Add simple example

---
+++ examples/simple.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/joushou/serve2"
+	"net"
+)
+
+//
+// To test:
+//   nc localhost 8080
+//
+// ... And then write "ECHO" or "DISCARD", followed by return, and what you
+// want echooed or discarded
+
+func main() {
+
+	l, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	server := serve2.New()
+
+	// These two are silly, and requires that you write "ECHO" or "DISCARD" when
+	// the connection is opened to recognize the protocol, as neither of these
+	// actually have any initial request or handshake.
+	echoHandler := serve2.NewEchoHandler()
+	discardHandler := serve2.NewDiscardHandler()
+
+	server.AddHandlers(echoHandler, discardHandler)
+	server.Serve(l)
+}
