8ad86e82bb9e93e96e7b2d28d7051d6e64c4c2a0
Kenny Levinsen <kl@codesealer.com>
2015-07-31 17:29:55 +0200

    Unfortunately, I have to go back to the previous handling :(

--- proto/proxy.go
+++ proto/proxy.go
@@ -2,51 +2,34 @@ package proto
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"io"
 	"net"
-
-	"github.com/joushou/serve2/utils"
+	"sync"
 )
 
 // proxy takes a ProxyConn "a", and a destination "dest" to dial, and forwards
 // traffic between the connections. Only supports "TCP" at the current time.
 func proxy(a net.Conn, proto, dest string) error {
-	if proto != "tcp" {
-		return errors.New("proxy only supports TCP for the time being")
-	}
-
-	aproxy, ok := a.(*utils.ProxyConn)
-	if !ok {
-		return errors.New("unable to convert source into *net.ProxyConn")
-	}
-
-	atcp, ok := aproxy.Conn.(*net.TCPConn)
-	if !ok {
-		return errors.New("unable to convert source into *net.TCPConn")
-	}
-
 	b, err := net.Dial(proto, dest)
 	if err != nil {
 		return err
 	}
 
-	btcp, ok := b.(*net.TCPConn)
-	if !ok {
-		return errors.New("unable to convert destination into *net.TCPConn")
+	var closer sync.Once
+	closerFunc := func() {
+		a.Close()
+		b.Close()
 	}
 
 	go func() {
 		io.Copy(a, b)
-		atcp.CloseWrite()
-		btcp.CloseRead()
+		closer.Do(closerFunc)
 	}()
 
 	go func() {
 		io.Copy(b, a)
-		btcp.CloseWrite()
-		atcp.CloseRead()
+		closer.Do(closerFunc)
 	}()
 
 	return nil
